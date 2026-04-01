dbaf1e721ea8af5b732a5b40f1fd84b5956169e6
Maksym <maksym@MacBook-Air-Maksym.local>
2026-04-01 13:24:26 +0300

    removed venv from repo, added gitignore

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -4,7 +4,7 @@ from psycopg2.extras import RealDictCursor
 
 app = Flask(__name__)
 
-# Налаштування підключення до PostgreSQL
+
 DB_CONFIG = {
     'host': 'localhost',
     'database': 'coinops',
@@ -13,13 +13,10 @@ DB_CONFIG = {
 }
 
 def get_db():
-    # Підключаємось до бази даних
     return psycopg2.connect(**DB_CONFIG)
 
 @app.route('/save', methods=['POST'])
 def save_rates():
-    # Цей маршрут буде викликати VM2 проксі
-    # щоб зберегти курси в БД
     from flask import request
     data = request.json
     
@@ -40,8 +37,6 @@ def save_rates():
 
 @app.route('/history', methods=['GET'])
 def get_history():
-    # Цей маршрут буде викликати VM1 Flask
-    # щоб отримати історичні дані для вкладки 2
     conn = get_db()
     cur = conn.cursor(cursor_factory=RealDictCursor)
     
--- proxy/main.go
+++ proxy/main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-// Це структура — опис як виглядають дані від НБУ
+
 type Rate struct {
 	CC   string  `json:"cc"`
 	Txt  string  `json:"txt"`
@@ -16,7 +16,6 @@ type Rate struct {
 }
 
 func getRates(w http.ResponseWriter, r *http.Request) {
-	// Йдемо до НБУ API
 	resp, err := http.Get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
 	if err != nil {
 		http.Error(w, "Помилка запиту до НБУ", http.StatusInternalServerError)
@@ -24,18 +23,16 @@ func getRates(w http.ResponseWriter, r *http.Request) {
 	}
 	defer resp.Body.Close()
 
-	// Читаємо відповідь
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		http.Error(w, "Помилка читання відповіді", http.StatusInternalServerError)
 		return
 	}
 
-	// Парсимо JSON
+
 	var allRates []Rate
 	json.Unmarshal(body, &allRates)
 
-	// Фільтруємо тільки USD, EUR, GBP
 	needed := map[string]bool{"USD": true, "EUR": true, "GBP": true}
 	var filtered []Rate
 	for _, rate := range allRates {
@@ -44,13 +41,11 @@ func getRates(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// Відповідаємо Flask у форматі JSON
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(filtered)
 }
 
 func main() {
-	// Реєструємо маршрут /rates
 	http.HandleFunc("/rates", getRates)
 	
 	fmt.Println("Проксі сервіс запущено на порту 8080")
