f2c871bcea901997956650821858c389b027c0fd
Maksym <dem2bb1@gmail.com>
2026-04-01 14:50:20 +0000

    added rabitmq publisher to proxy

--- proxy/go.mod
+++ proxy/go.mod
@@ -1,3 +1,5 @@
 module proxy
 
 go 1.22.2
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
--- proxy/main.go
+++ proxy/main.go
@@ -6,8 +6,9 @@ import (
 	"io"
 	"log"
 	"net/http"
-)
 
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 type Rate struct {
 	CC   string  `json:"cc"`
@@ -15,7 +16,63 @@ type Rate struct {
 	Rate float64 `json:"rate"`
 }
 
+var rabbitConn *amqp.Connection
+var rabbitChannel *amqp.Channel
+
+func connectRabbitMQ() {
+	// Підключаємось до RabbitMQ на VM4
+	conn, err := amqp.Dial("amqp://coinops:coinops123@192.168.56.104:5672/")
+	if err != nil {
+		log.Printf("Помилка підключення до RabbitMQ: %s", err)
+		return
+	}
+	rabbitConn = conn
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Printf("Помилка відкриття каналу: %s", err)
+		return
+	}
+	rabbitChannel = ch
+
+	// Створюємо чергу якщо не існує
+	ch.QueueDeclare("rates", false, false, false, false, nil)
+	log.Println("Підключено до RabbitMQ")
+}
+
+func publishToQueue(rates []Rate) {
+	if rabbitChannel == nil {
+		log.Println("RabbitMQ не підключений")
+		return
+	}
+
+	// Конвертуємо в JSON
+	body, err := json.Marshal(rates)
+	if err != nil {
+		log.Printf("Помилка конвертації: %s", err)
+		return
+	}
+
+	// Публікуємо в чергу
+	err = rabbitChannel.Publish(
+		"",      // exchange
+		"rates", // queue name
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		},
+	)
+	if err != nil {
+		log.Printf("Помилка публікації: %s", err)
+		return
+	}
+	log.Println("Повідомлення відправлено в чергу")
+}
+
 func getRates(w http.ResponseWriter, r *http.Request) {
+	// Йдемо до НБУ API
 	resp, err := http.Get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
 	if err != nil {
 		http.Error(w, "Помилка запиту до НБУ", http.StatusInternalServerError)
@@ -25,14 +82,14 @@ func getRates(w http.ResponseWriter, r *http.Request) {
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		http.Error(w, "Помилка читання відповіді", http.StatusInternalServerError)
+		http.Error(w, "Помилка читання", http.StatusInternalServerError)
 		return
 	}
-
 
 	var allRates []Rate
 	json.Unmarshal(body, &allRates)
 
+	// Фільтруємо USD, EUR, GBP
 	needed := map[string]bool{"USD": true, "EUR": true, "GBP": true}
 	var filtered []Rate
 	for _, rate := range allRates {
@@ -41,13 +98,19 @@ func getRates(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	// Публікуємо в RabbitMQ асинхронно
+	go publishToQueue(filtered)
+
+	// Повертаємо дані Flask
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(filtered)
 }
 
 func main() {
+	// Підключаємось до RabbitMQ при старті
+	connectRabbitMQ()
+
 	http.HandleFunc("/rates", getRates)
-	
 	fmt.Println("Проксі сервіс запущено на порту 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
