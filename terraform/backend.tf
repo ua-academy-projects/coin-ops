4e65a3d0df2455d17c254f324fd24280fdeea024
Roman Kurdupel <rkurdupel@Romans-MacBook-Pro.local>
2026-05-19 02:47:40 +0300

    feat: add azure backend for terraform state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,23 +7,23 @@
 # }
 
 
-terraform {
-  backend "s3" {
-    bucket  = "coinops-dev-tf-state"
-    key     = "terraform/aws/state/default.tfstate"
-    region  = "eu-central-1"
-    encrypt = true
-  }
-}
-
 # terraform {
-#   backend "azurerm" {
-#     resource_group_name = "devops-dev-rg"
-#     storage_account_name = "coinopsdevtfstate"
-#     container_name = "tfstate"
-#     key = "terraform/azure/state/default.tfstate"
+#   backend "s3" {
+#     bucket  = "coinops-dev-tf-state"
+#     key     = "terraform/aws/state/default.tfstate"
+#     region  = "eu-central-1"
+#     encrypt = true
 #   }
 # }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name = "coinops-state-rg"
+    storage_account_name = "coinopsdevtfstate"
+    container_name = "tfstate"
+    key = "terraform/azure/state/default.tfstate"
+  }
+}
 
 #   terraform init -reconfigure
 # DESTROY BEFORE CHANGING BACKGROUND
