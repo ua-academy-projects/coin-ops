284f0d3079d03f89b2908634b4b9419a372e9dc5
artur-vol <volunetsartur@gmail.com>
2026-05-27 00:08:19 +0300

    remove azure locals artifacts from main

--- terraform/cloud/main.tf
+++ terraform/cloud/main.tf
@@ -6,7 +6,7 @@ module "azure_network" {
   source = "./modules/azure/network"
   count  = local.networks_by_cloud.azure != null ? 1 : 0
 
-  resource_group_name = local.azure.resource_group_name
+  resource_group_name = var.azure_resource_group_name
   network             = local.networks_by_cloud.azure
 }
 
@@ -14,8 +14,8 @@ module "azure_security" {
   source = "./modules/azure/security"
   count  = local.networks_by_cloud.azure != null && length(local.workloads_by_cloud.azure) > 0 && length(local.default_cloud_security_rules) > 0 && local.default_cloud == "azure" ? 1 : 0
 
-  resource_group_name = local.azure.resource_group_name
-  location            = local.azure.location
+  resource_group_name = var.azure_resource_group_name
+  location            = var.azure_location
   subnet_ids          = module.azure_network[0].subnetwork_ids
   subnets             = local.networks_by_cloud.azure.subnets
   workloads           = local.workloads_by_cloud.azure
@@ -26,10 +26,10 @@ module "azure_instances" {
   source = "./modules/azure/instances"
   count  = local.networks_by_cloud.azure != null && length(local.workloads_by_cloud.azure) > 0 ? 1 : 0
 
-  resource_group_name            = local.azure.resource_group_name
-  location                       = local.azure.location
+  resource_group_name            = var.azure_resource_group_name
+  location                       = var.azure_location
   ssh_public_key_path            = pathexpand(var.ssh_public_key_path)
-  key_vault_name                 = local.azure.key_vault_name
+  key_vault_name                 = var.azure_key_vault_name
   subnet_ids                     = module.azure_network[0].subnetwork_ids
   application_security_group_ids = try(module.azure_security[0].application_security_group_ids, {})
   workloads                      = local.workloads_by_cloud.azure
@@ -39,13 +39,13 @@ module "azure_sql" {
   source = "./modules/azure/sql"
   count  = local.networks_by_cloud.azure != null && local.default_cloud_sql != null && local.default_cloud == "azure" ? 1 : 0
 
-  resource_group_name   = local.azure.resource_group_name
-  location              = local.azure.location
+  resource_group_name   = var.azure_resource_group_name
+  location              = var.azure_location
   network_name          = module.azure_network[0].network_name
   network_id            = module.azure_network[0].network_id
   network_cidr          = local.networks_by_cloud.azure.cidr
-  key_vault_name        = local.azure.key_vault_name
-  db_password_secret_id = local.normalized_secrets["db_password"].secret_id
+  key_vault_name        = var.azure_key_vault_name
+  db_password_secret_id = var.secrets["db_password"].secret_id
   placement             = local.default_cloud_sql.placement
   instance              = local.default_cloud_sql.instance
   database              = local.default_cloud_sql.database
@@ -56,7 +56,7 @@ module "azure_routing" {
   source = "./modules/azure/routing"
   count  = local.networks_by_cloud.azure != null && length(local.workloads_by_cloud.azure) > 0 && local.default_cloud_nat_route != null && local.default_cloud == "azure" ? 1 : 0
 
-  resource_group_name = local.azure.resource_group_name
+  resource_group_name = var.azure_resource_group_name
   route = {
     name              = local.default_cloud_nat_route.name
     destination_range = local.default_cloud_nat_route.destination_range
@@ -107,7 +107,7 @@ module "gcp_secrets" {
   source = "./modules/gcp/secrets"
   count  = local.networks_by_cloud.gcp != null && length(local.workloads_by_cloud.gcp) > 0 && local.default_cloud == "gcp" ? 1 : 0
 
-  secrets          = local.normalized_secrets
+  secrets          = var.secrets
   workloads        = local.workloads_by_cloud.gcp
   service_accounts = module.gcp_instances[0].service_accounts
 }
@@ -118,7 +118,7 @@ module "gcp_sql" {
 
   placement             = local.default_cloud_sql.placement
   network_name          = module.gcp_network[0].network_name
-  db_password_secret_id = local.normalized_secrets["db_password"].secret_id
+  db_password_secret_id = var.secrets["db_password"].secret_id
 
   instance = local.default_cloud_sql.instance
   database = local.default_cloud_sql.database
