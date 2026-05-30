96bde7ad0c790766cd9bb23ce9e6dc7164b6f486
artur-vol <volunetsartur@gmail.com>
2026-05-30 15:55:54 +0300

    add main file for aws sql module

---
+++ terraform/cloud/modules/aws/sql/main.tf
@@ -0,0 +1,33 @@
+# main.tf
+
+resource "aws_db_subnet_group" "this" {
+  name = "${local.instance.identifier}-subnets"
+  subnet_ids = values(var.private_subnet_ids)
+}
+
+resource "aws_security_group" "this" {
+  name = "${local.instance.identifier}-db"
+  vpc_id = var.network_id
+}
+
+resource "aws_db_instance" "this" {
+  identifier = local.instance.identifier
+  engine = local.instance.engine
+  engine_version         = local.instance.engine_version
+  instance_class         = local.instance.instance_class
+  allocated_storage      = local.instance.allocated_storage
+  storage_type           = local.instance.storage_type
+  multi_az               = local.instance.multi_az
+  db_name                = local.instance.database_name
+  username               = local.instance.username
+
+  # FIXME: we should use password from secrets here
+  password               = "CHANGE_ME_TEMP"
+
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [aws_security_group.this.id]
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  deletion_protection    = local.instance.deletion_protection
+  backup_retention_period = local.instance.backup_retention
+}
