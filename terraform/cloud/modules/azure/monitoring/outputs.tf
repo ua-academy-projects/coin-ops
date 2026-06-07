6e7a2b8c91aba42d72f2e2b0a0781e16a3de2b8e
artur-vol <volunetsartur@gmail.com>
2026-06-08 00:46:16 +0300

    add law creation

---
+++ terraform/cloud/modules/azure/monitoring/outputs.tf
@@ -0,0 +1,13 @@
+# outputs.tf
+
+output "workspace_id" {
+  value = azurerm_log_analytics_workspace.this.id
+}
+
+output "workspace_name" {
+  value = azurerm_log_analytics_workspace.this.name
+}
+
+output "workspace_customer_id" {
+  value = azurerm_log_analytics_workspace.this.workspace_id
+}
---
+++ terraform/cloud/modules/azure/monitoring/variables.tf
@@ -0,0 +1,13 @@
+# variables.tf
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
