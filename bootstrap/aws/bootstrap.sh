9f9f51e63ddbdb53a69cc515238ee7fef7f8b9ec
Marta Penina <marta.penina.academic@gmail.com>
2026-06-17 10:10:07 +0300

    fix: restore missing EKS permissions block lost in earlier edit

--- bootstrap/aws/bootstrap.sh
+++ bootstrap/aws/bootstrap.sh
@@ -218,6 +218,28 @@ cat > "$POLICY_FILE" <<'POLICY'
       "Resource": "*"
     },
     {
+      "Sid": "EKS",
+      "Effect": "Allow",
+      "Action": [
+        "eks:CreateCluster",
+        "eks:DeleteCluster",
+        "eks:DescribeCluster",
+        "eks:ListClusters",
+        "eks:UpdateClusterConfig",
+        "eks:UpdateClusterVersion",
+        "eks:TagResource",
+        "eks:UntagResource",
+        "eks:ListTagsForResource",
+        "eks:CreateNodegroup",
+        "eks:DeleteNodegroup",
+        "eks:DescribeNodegroup",
+        "eks:ListNodegroups",
+        "eks:UpdateNodegroupConfig",
+        "eks:UpdateNodegroupVersion"
+      ],
+      "Resource": "*"
+    },
+    {
       "Sid": "EKSServiceLinkedRole",
       "Effect": "Allow",
       "Action": "iam:CreateServiceLinkedRole",
