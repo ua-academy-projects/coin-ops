14f63ba352a14fbd90e340e213a77e7a337d6d16
vitus <ua.orc.asd@gmail.com>
2026-06-17 13:56:48 +0300

    Revert node size to 2

--- eks-migration/terraform/eks.tf
+++ eks-migration/terraform/eks.tf
@@ -45,9 +45,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      min_size     = 3
+      min_size     = 2
       max_size     = 5
-      desired_size = 3
+      desired_size = 2
 
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
