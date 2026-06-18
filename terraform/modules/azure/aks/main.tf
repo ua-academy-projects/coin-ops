b4bfb7ddf9249b414f39a11f7e52c06b97437adb
Roman Kurdupel <rkurdupel@Romans-MacBook-Pro.local>
2026-06-18 14:21:18 +0300

    feat: configure aks module

--- terraform/modules/azure/aks/main.tf
+++ terraform/modules/azure/aks/main.tf
@@ -1,45 +1,44 @@
 # aks - azure kubernetes cluster
 resource "azurerm_resource_group" "aks" {
-    name = "${var.name_prefix}-aks-rg"
-    location = var.location
+  name     = "${var.name_prefix}-aks-rg"
+  location = var.location
 }
 
 resource "azurerm_kubernetes_cluster" "this" {
-    name = "${var.name_prefix}-aks"
-    location = azurerm_resource_group.aks.location
-    resource_group_name = azurerm_resource_group.aks.name
-    # dns_prefix generates endpoint (coinops-aks-abc123.eastdenmark) that later is used to connect AKS to k3s config (to use helm upgrade, kubectl (AKS only))
-    dns_prefix = "${var.name_prefix}-aks"
+  name                = "${var.name_prefix}-aks"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  # dns_prefix generates endpoint (coinops-aks-abc123.eastdenmark) that later is used to connect AKS to k3s config (to use helm upgrade, kubectl (AKS only))
+  dns_prefix = "${var.name_prefix}-aks"
 
-    # create a vm for AKS
-    default_node_pool {
-        name = "default"
-        node_count = 1
-        vm_size = "Standard_B2s_v2"
-    }
-    
-    # permission to manage azure resources
-    identity {
-        type = "SystemAssigned"
-    }
+  # create a vm for AKS
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s_v2"
+  }
+
+  # permission to manage azure resources
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
 resource "helm_release" "jenkins" {
-    name = "jenkins"
-    repository = "https://charts.jenkins.io"
-    chart = "jenkins"
-    namespace = "cicd"
-    create_namespace = true
+  name             = "jenkins"
+  repository       = "https://charts.jenkins.io"
+  chart            = "jenkins"
+  namespace        = "cicd"
+  create_namespace = true
 
-    depends_on = [azurerm_kubernetes_cluster.this]
+  values = [
+    templatefile("${path.module}/jenkins-values.yaml.tftpl", {
+      casc_config = templatefile("${path.module}/jenkins-casc.yaml.tftpl", {
+        github_repo_url = var.github_repo_url
+        git_branch      = var.git_branch
+      })
+    })
+  ]
 
-}
-
-resource "cloudflare_record" "jenkins" {
-    zone_id = var.cloudflare_zone_id
-    name = "jenkins"
-    type = "CNAME"
-    content = azurerm_kubernetes_cluster.this.fqdn
-    ttl = 60    #  # dns cache time 60 seconds (user opens app.coin-ops.pp.ua => dns resolver asks cloudflare where to go it redirects to alb and saves that answer for 60 sec)
-    proxied = false
+  depends_on = [azurerm_kubernetes_cluster.this]
 }
--- terraform/modules/azure/aks/variables.tf
+++ terraform/modules/azure/aks/variables.tf
@@ -1,12 +1,22 @@
 variable "name_prefix" {
-    type = string
+  type = string
 }
 
 variable "location" {
-    type = string
-    default = "denmarkeast"
+  type    = string
+  default = "denmarkeast"
 }
 
 variable "cloudflare_zone_id" {
-    type = string
+  type = string
+}
+
+variable "github_repo_url" {
+  type    = string
+  default = "https://github.com/ua-academy-projects/coin-ops.git"
+}
+
+variable "git_branch" {
+  type    = string
+  default = "*/kurdupel"
 }
